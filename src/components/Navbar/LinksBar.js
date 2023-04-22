9990b0037ad8d778be59f696195d20f8acefba56
unknown <pavel.vynn@gmail.com>
2023-04-22 12:15:58 +0300

    check where links is going

--- src/components/Navbar/LinksBar.js
+++ src/components/Navbar/LinksBar.js
@@ -5,12 +5,12 @@ export default function LinksBar({styles, setWebHref}) {
   return (
     <>
       <div className={styles.links_bar__inner}>
-        <Link to="/movie-web" onClick={(e) => setWebHref(e.target.href)}>Головна</Link>
-        <Link to="/movie-web" onClick={(e) => setWebHref(e.target.href)}>Новинки</Link>
-        <Link to="/movie-web/films" onClick={(e) => setWebHref(e.target.href)}>Фільми</Link>
-        <Link to="/movie-web/serials" onClick={(e) => setWebHref(e.target.href)}>Серіали</Link>
-        <Link to="/movie-web/anime" onClick={(e) => setWebHref(e.target.href)}>Аніме</Link>
-        <Link to="/movie-web/favourites" onClick={(e) => setWebHref(e.target.href)}>Збережені</Link>
+        <Link to="/movie-web" onClick={(e) => {setWebHref(e.target.href); console.log(e)}}>Головна</Link>
+        <Link to="/movie-web" onClick={(e) => {setWebHref(e.target.href); console.log(e)}}>Новинки</Link>
+        <Link to="/movie-web/films" onClick={(e) => {setWebHref(e.target.href); console.log(e)}}>Фільми</Link>
+        <Link to="/movie-web/serials" onClick={(e) => {setWebHref("http://pavelvynnpv.github.io/movie-web/serials"); console.log(e)}}>Серіали</Link>
+        <Link to="/movie-web/anime" onClick={(e) => {setWebHref(e.target.href); console.log(e)}}>Аніме</Link>
+        <Link to="/movie-web/favourites" onClick={(e) => {setWebHref(e.target.href); console.log(e)}}>Збережені</Link>
       </div>
     </>
   );
